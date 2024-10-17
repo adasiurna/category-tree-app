63c05ea8bba0b138fbf0dffed8bc23d03a104d43
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2024-10-17 14:54:53 +0300

    add / remove categories

--- src/data/components/CategoryTree.tsx
+++ src/data/components/CategoryTree.tsx
@@ -14,10 +14,52 @@ const CategoryTree: React.FC<ICategoryTreeProps> = ( { data } ) => {
 
   const [categories, setCategories] = useState<ICategory[]>(data);
 
+  const addNode = (parentId: number, nodeName: string) => {
+    const addCategory = (category: ICategory): ICategory => {
+      if (category.id === parentId) {
+        const newCategory: ICategory = {
+          id: new Date().getTime(), // unique id
+          name: nodeName,
+          children: []
+        };
+        return { ...category, children: [...category.children, newCategory] };
+      } else {
+        return { ...category, children: category.children.map(addCategory) };
+      }
+    };
+
+    setCategories(categories.map(addCategory));
+  };
+
+  const removeNode = (nodeId: number) => {
+    const removeCategory = (category: ICategory): ICategory | null => {
+      if (category.id === nodeId) {
+        return null;
+      }
+      return {
+        ...category,
+        children: category.children
+          .map(removeCategory)
+          .filter((child) => child !== null) as ICategory[],
+      };
+    };
+
+    setCategories(categories.map(removeCategory).filter(Boolean) as ICategory[]);
+  };
+
+  const handleAdd = (id:number) => {
+    const categoryName = prompt('Enter category name');
+    if (id && categoryName ) {
+      addNode(id, categoryName);
+    }
+  }
+
   const renderTree = (category: ICategory) => {
     return (
       <li key={category.id}>
         - {category.name}
+        <button onClick={() => removeNode(category.id)}>Remove</button>
+        <button onClick={() => handleAdd(category.id)}>Add</button>
         {category.children.length > 0 && (
           <ul>{category.children.map(renderTree)}</ul>
         )}
@@ -27,7 +69,6 @@ const CategoryTree: React.FC<ICategoryTreeProps> = ( { data } ) => {
 
   return (
     <div>
-      <button>Add Node</button>
       <ul>{categories.map(renderTree)}</ul>
     </div>
   )
